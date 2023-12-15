a12bc4ecbd0f3fee6d6a3a4385b68c052f6df304
tecsense19 <tecsense19@gmail.com>
2023-12-15 13:05:01 +0530

    comment remove

--- resources/views/front/chat/chat.blade.php
+++ resources/views/front/chat/chat.blade.php
@@ -896,8 +896,8 @@ function unlikedMessage(message, messageId) {
 <script>  
 $('.remove-chat').click(function(e) {
     var chatid = $(this).data('bs-chatid');
-    // var url = "{{ route('chat.delete', ['id' => ':chatid'], [], true) }}";
-    var url = "{{ route('chat.delete', ['id' => ':chatid']) }}";
+    var url = "{{ route('chat.delete', ['id' => ':chatid'], [], true) }}";
+    // var url = "{{ route('chat.delete', ['id' => ':chatid']) }}";
     url = url.replace(':chatid', chatid);
 
     e.preventDefault();
