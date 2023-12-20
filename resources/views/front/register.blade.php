dc34ca7bdfc817cce0f7806e273ed07faaf9048b
tecsense19 <tecsense19@gmail.com>
2023-12-20 12:38:05 +0530

    title change

--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>halochat.AI | chat and connect with AI companions</title>
+  <title>vice.app | chat and connect with AI companions</title>
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
