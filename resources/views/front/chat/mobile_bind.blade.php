29b6725a96e3a7dbd6c02dec040e600f4c0f79e2
tecsense19 <tecsense19@gmail.com>
2024-01-02 12:36:04 +0530

    audio file with play button

--- resources/views/front/chat/mobile_bind.blade.php
+++ resources/views/front/chat/mobile_bind.blade.php
@@ -8,15 +8,32 @@
             @foreach ($getAllReciverUser as $keys => $chat_user)  
                 @if($chat_user->sender_id == $chat_user->user_id)
                     @if(!empty($chat_user->message_text))
+
+                    <?php
+                    //$chat_user = // ... your $chat_user object;
+                    $profilename = \App\Models\Profile::where('profile_id', $chat_user->profile_id)->first();
+                    // Check if {{first_name}} is present in the message text
+                        if (str_contains($chat_user->message_text, '{{first_name}}')) {
+                            // Replace {{first_name}} with the actual first name
+                            $messageText = str_replace('{{first_name}}', $profilename->name, $chat_user->message_text);
+                        } else{
+                            $messageText = $chat_user->message_text;
+                        }
+                    
+                 
+                    // Output the result with line breaks converted to HTML breaks 
+                    //  echo nl2br($messageText); 
+                    ?>
                         <div class="col-12 mb-2">
                             <div class="have_we_met">
                                 <div class="chat_content_box">
                                     <div class="">
-                                        <p id="message" class="sender_message_{{ $keys }}"><?= nl2br($chat_user->message_text) ?></p>
+                                        <p id="message" class="sender_message_{{ $keys }}"><?= nl2br($messageText) ?></p>
                                     </div>
-                                    <div class="volume">
-                                        <span>
-                                            <svg id="play-icon" width="20 " class="text-[#C14DA0]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"></path></svg>
+                                    <div class="volume" data-id="audioPlayer_{{ $keys }}">
+                                    <audio class="audioPlayer_{{ $keys }}" src="{{ URL::to($chat_user->voicemessagepath) }}"></audio> 
+                                    <span id="play-icon" style="cursor: pointer;">
+                                            <svg width="20 " class="text-[#C14DA0]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"></path></svg>
                                         </span>
                                     </div>
                                 </div>
@@ -153,3 +170,20 @@
         </div> -->
     </div>
 </div>
+
+
+<script>
+    $(document).ready(function () {
+        $('.volume').click(function () {
+            var ids = $(this).data('id');
+            var audio = $('.'+ids)[0];            
+            if (audio.paused) {
+                audio.play();
+                // $('#play-icon').html('<svg width="20" class="text-[#C14DA0]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"></path></svg>');
+            } else {
+                audio.pause();
+                // $('#play-icon').html('<svg width="20" class="text-[#C14DA0]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"></path></svg>');
+            }
+        });
+    });
+</script>
